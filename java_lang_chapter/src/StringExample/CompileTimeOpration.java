519687609597770d2e26ebc2d83ed5ab109d4705
sachin <abc@gmail.com>
2022-08-02 21:43:09 +0530

    string object related programs

---
+++ java_lang_chapter/src/StringExample/CompileTimeOpration.java
@@ -0,0 +1,15 @@
+package StringExample;
+
+public class CompileTimeOpration {
+
+	public static void main(String[] args) {
+		
+		String b = new String("RightWayLearning");
+		final String c = "Right";
+		final String d = "Way";
+		final String e = "Learning";
+		 String f = c + d + e;
+		
+		String g = "RightWayLearning";
+	}
+}
